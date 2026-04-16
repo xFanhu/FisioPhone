b458ed9dff44844e645022f7f54a2bd07666bf95
Inigo Garcia Arsuaga <inigar15@gmail.com>
2026-04-16 09:59:21 +0200

    refactor: Simplify MainActivity by removing unused Compose code and restore setContentView

--- app/src/main/java/com/example/fisiophone/MainActivity.kt
+++ app/src/main/java/com/example/fisiophone/MainActivity.kt
@@ -2,46 +2,10 @@ package com.example.fisiophone
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.fisiophone.ui.theme.FisioPhoneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            FisioPhoneTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FisioPhoneTheme {
-        Greeting("Android")
-    }
-}
+}    
