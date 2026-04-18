f84f6cb540280ec86c7e122e3c8ba54137c9118a
Inigo Garcia Arsuaga <inigar15@gmail.com>
2026-04-18 19:49:14 +0200

    feat: Update drawable resources and layout for MenuActivity, add CardView, and include CardView dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.annotation)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
